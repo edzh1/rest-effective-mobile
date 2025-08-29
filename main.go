1eef18eba382dec769efd191f1436ba7530a9925
Дмитрий Ковтун <dkovtun.work@gmail.com>
2025-08-29 12:51:05 +0300

    project bootstrap

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("hello"))
+	})
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal("can't start server " + err.Error())
+	}
 }
